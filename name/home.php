6416acf3b4d622e11835c97ee25d3dd7ff192620
Quincy Bock <QuincyBock@post.harvard.edu>
2014-04-07 21:49:30 +0000

    home and notifications scaffolded

--- name/home.php
+++ name/home.php
@@ -5,28 +5,97 @@
 <html>
 <head>
     <title>NAME</title>
-<!--     <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script> -->
-<!--     <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jqueryui/1/jquery-ui.min.js"></script> -->
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <script src="javascript/raphael-min.js"></script>
+    <script src="http://malsup.github.io/jquery.corner.js"></script>
+
+<!--
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jqueryui/1/jquery-ui.min.js"></script>
+-->
    	<link type="stylesheet" rel="stylesheet" href="css/style.css" />
    	<link type="stylesheet" rel="stylesheet" href="css/fonts.css" />
-	<link href='http://fonts.googleapis.com/css?family=Dosis:200,300,400,500,600,700,800|Roboto+Condensed:300italic,400italic,700italic,400,300,700' rel='stylesheet' type='text/css'>
-	<meta name="viewport" content="width=device-width, initial-scale=1, minimal-ui">
+	<meta name="viewport" content="width=device-width, initial-scale=1, minimal-ui, maximum-scale=1.0">
 </head>
 <body>
 
-	<nav class="topNav">
-		<h1>HOME</h1>
-	</nav>
-	<nav class="bottomNav">
+
+	
+	<section class="menuItem" id="HOME">
+		<article id="progress">
+			<center><h2 id="estimatedGoal">Estimated Goal: <span id="estimatedGoal">35</span> min</h2></center>
+			<br>
+			<center><div id="progressCanvas"> </div></center>
+			<center><h2 id="yourTotalToday">12 min</h2></center>
+		</article>
+		<article id="notification">
+			<h1 class="smaller" id="mostRecent">ASK GOOD QUESTIONS</h1>
+			<p id="mostRecent">Ask John if there was any time when 
+he felt overwhelmed today.</p>
+		</article>
 		
+	</section>
+	
+	
+	<section class="menuItem" id="STATS">
+
+	</section>
+	
+	
+	<section class="menuItem" id="SETTINGS">
+
+	</section>
+	
+	
+	<section class="menuItem" id="NOTIFICATIONS">
+		<article id="notificationHolder">
+			<article class="notification">
+				<h1 class="smaller">ASK GOOD QUESTIONS</h1>
+				<p>Ask John if there was any time when 
+	he felt overwhelmed today.</p>
+			</article>
+			<article class="notification coupon">
+				<h1 class="smaller">ASK GOOD QUESTIONS</h1>
+				<p>Ask John if there was any time when 
+	he felt overwhelmed today.</p>
+			</article>
+			<article class="notification">
+				<h1 class="smaller">ASK GOOD QUESTIONS</h1>
+				<p>Ask John if there was any time when 
+	he felt overwhelmed today.</p>
+			</article>
+			<article class="notification coupon">
+				<h1 class="smaller">ASK GOOD QUESTIONS</h1>
+				<p>Ask John if there was any time when 
+	he felt overwhelmed today.</p>
+			</article>
+			<article class="notification">
+				<h1 class="smaller">ASK GOOD QUESTIONS</h1>
+				<p>Ask John if there was any time when 
+	he felt overwhelmed today.</p>
+			</article>
+			<article class="notification coupon">
+				<h1 class="smaller">ASK GOOD QUESTIONS</h1>
+				<p>Ask John if there was any time when 
+	he felt overwhelmed today.</p>
+			</article>
+		
+		</article>
+	</section>
+	
+	
+		<nav class="topNav">
+		<h1 id="pageName">HOME</h1>
+	</nav>
+	
+	<nav class="bottomNav">
 		<section class="iconHolder">
-			<img class="navIcons" id="home" src="images/homeIcon.png" />
-			<img class="navIcons" id="stats" src="images/statsIcon.png" />
-			<img class="navIcons" id="settings" src="images/settingsIcon.png" />
-			<img class="navIcons" id="notifications" src="images/notificationsIcon.png" />
+			<img class="navIcons" id="home" name="HOME" src="images/homeIcon.png" />
+			<img class="navIcons" id="stats" name="STATS" src="images/statsIcon.png" /></a>
+			<img class="navIcons" id="settings" name="SETTINGS" src="images/settingsIcon.png" />
+			<img class="navIcons" id="notifications" name="NOTIFICATIONS" src="images/notificationsIcon.png" />
 		</section>	
 		<img class="addButton" src="images/addButton.png" />
-
 	</nav>
 	
 <script>
--- name/javascript/home.js
+++ name/javascript/home.js
@@ -1,335 +1,101 @@
 $(document).ready(function() {
-
-	var username = "";
+	$(window).scrollTop(0);
+	$('section.menuItem').hide();
+	$('section#HOME').show();
+	drawCircle();
 	
-	$('#usernameInput').keyup(function(){
-		username = $(this).val();		
+	$('section.iconHolder img').click(function(){
+		$('h1#pageName').text($(this).attr("name"));
+		$('section.menuItem').hide();
+		var thisSection = $(this).attr("name");
+		var showThisSection = "section#" + thisSection;
+		console.log(showThisSection);
+		$(showThisSection).show();
 	});
 	
-	$('#passwordInput').keyup(function(){
-		password = $(this).val();		
+	
+	$('section.iconHolder img#home').click(function(){
+		$("#progressCanvas svg").remove();
+		drawCircle();
 	});
 	
-    $('.my_modal_open').click(function(){
-        $('#my_modal').popup({
-            'autoopen': true
-        });
-    });
-    
-    $('#container').fullContent({
-		stages: 'section',
-	    mapPosition: [{v: 1, h: 1}, {v:1 , h: 2}, {v:1 , h: 3}, {v:1 , h: 4},  {v:1 , h: 5}, {v:1 , h: 6}, {v:1 , h: 7}, {v:1 , h: 8}, {v:1 , h: 9}, {v:1 , h: 10}, {v:1 , h: 11}],
-	    stageStart: 1,
-		idComplement: 'page_'
-	}); 
-	
-	$('div#loginBox').hide();
-	$('#pageCover').hide();
-	$('#mistakeBox').hide();
-	$('#graphBoxPageCover').hide();
-	$('#graphBox').hide();
-	
-	$('button#loginButton').click(function(){
-		$('div#loginBox').slideToggle();
-	});
-	
-	$('#closeLogin').click(function(){
-		$('div#loginBox').slideToggle();
-	});
-	
-	$("#loginSubmitButton").click(function(){
-		if((username == "Quincy" && password == "m") || (username == "Mauricio" && password == "mauricio") || (username == "Sample") || (username == "sample") ){
-			$.ajax({
-				type: "POST",
-				url: "homeData.php",
-				data: { userName: username},
-				success: function(data) {
-					document.getElementById("myDataHolder").innerHTML = data;
-				}
-			});
-			$.ajax({
-				type: "POST",
-				url: "suggestionData.php",
-				data: { userName: username},
-				success: function(data) {
-					document.getElementById("suggestionsHolder").innerHTML = data;
-				}
-			});
-		}
-		else if(username == "" && password == ""){
-			event.preventDefault();
-			alert("You need to fill in a username and password.");
-		}
-		else{
-			event.preventDefault();
-			alert("Your username and password do not match.");
-		}
-		console.log(username, password);
-	});
-	
-
-	$(document).on("click", "#myDataHolder div", function(){
-		var whatsInside = this.innerHTML;
- 		var updated = whatsInside.replace('<center>', '');
- 		var updated2 = updated.replace('</center>', '');
- 		var trimed = $.trim(updated2); 
-		console.log(trimed);
-		 	
-		$.ajax({
-			type: "POST",
-			url: "makeGraphHome.php",
-			data: { userNameVal: username, movieVal: trimed },
-			success: function(data) {
-				document.getElementById("graphBoxDataHolder").innerHTML = data;
-			}
-		});	
+	function drawCircle(){
+		var amount = 80;
+		var circleCenter = 125;
+		var circleSize = 100;
 		
-		document.getElementById("graphHomeMovieName").innerHTML = trimed;
-		$('#graphBoxPageCover').fadeIn();
-		$('#graphBox').slideDown();
-	});	
+		var archtype = Raphael("progressCanvas", 250, 250);
+		archtype.customAttributes.arc = function (xloc, yloc, value, total, R) {
+		    var alpha = 360 / total * value,
+		        a = (90 - alpha) * Math.PI / 180,
+		        x = xloc + R * Math.cos(a),
+		        y = yloc - R * Math.sin(a),
+		        path;
+		    if (total == value) {
+		        path = [
+		            ["M", xloc, yloc - R],
+		            ["A", R, R, 0, 1, 1, xloc - 0.01, yloc - R]
+		        ];
+		    } else {
+		        path = [
+		            ["M", xloc, yloc - R],
+		            ["A", R, R, 0, +(alpha > 180), 1, x, y]
+		        ];
+		    }
+		    return {
+		        path: path
+		    };
+		};
+		
+		//skinny lighter
+		var my_arc_bg3 = archtype.path().attr({
+		    "stroke": "#EEEEEE",
+		    "stroke-width": 1,
+		    "stroke-linecap":"round",
+		    arc: [circleCenter, circleCenter, 0, 100, circleSize-17]
+		});
+		
+		my_arc_bg3.animate({
+		    arc: [circleCenter, circleCenter, 100, circleSize, circleSize-17]
+		}, 0, "bounce");
+		
+		//slightly larger
+		var my_arc_bg2 = archtype.path().attr({
+		    "stroke": "#EEEEEE",
+		    "stroke-width": 20,
+		    "stroke-linecap":"round",
+		    arc: [circleCenter, circleCenter, 0, 100, circleSize+8]
+		});
+		
+		my_arc_bg2.animate({
+		    arc: [circleCenter, circleCenter, 100, circleSize, circleSize+8]
+		}, 0, "bounce");
+		
+		//darkest
+		var my_arc_bg1 = archtype.path().attr({
+		    "stroke": "#DADADA",
+		    "stroke-width": 22,
+		    "stroke-linecap":"round",
+		    arc: [circleCenter, circleCenter, 0, 100, circleSize]
+		});
+		
+		my_arc_bg1.animate({
+		    arc: [circleCenter, circleCenter, 100, circleSize, circleSize]
+		}, 0, "bounce");
 	
-	$('#closeGraph').click(function(){
-		$('#graphBoxPageCover').fadeOut();
-		$('#graphBox').slideUp();
-	});
-	
-	$('#beginMovieButton').click(function(){
-	    movie = $('#q').val();
-	    if(movie == ""){
-			event.preventDefault();
-		    alert("You need to input a movie!");
-	    }
-	    timer = 61;
-	    counting = true;
-	    $.ajax({
-	    	type: "POST",
-			url: "getTime.php",
-			success: function(data) {
-				startTime = data; 
-				console.log(startTime);
-			}
+		
+		//make an arc at 50,50 with a radius of 30 that grows from 0 to 40 of 100 with a bounce
+		//red
+		var my_arc = archtype.path().attr({
+		    "stroke": "#F82641",
+		    "stroke-width": 10,
+		    "stroke-linecap":"round",
+		    arc: [circleCenter, circleCenter, 0, circleSize, circleSize]
 		});
-    });
-    
-    $('#goBack').click(function(){
-	    $('#pageCover').fadeOut();
-	    $('#mistakeBox').slideUp();
-	    timer = 61;
-	    countDownDone = false;
-    });
-    
-    $('#moreTime').click(function(){
-	    $('#pageCover').fadeOut();
-	    $('#mistakeBox').slideUp();
-	    timer = 61;
-	    counting = true;
-	    countDownDone = false;
-	    $.ajax({
-	    	type: "POST",
-			url: "getTime.php",
-			success: function(data) {
-				startTime = data;
-				console.log(startTime); 
-			}
-		});
-    });
-    
-    $('#testForwardButton').click(function(){
-	   recievedData = true;
-	   console.log(recievedData);
-    });
-    
-    $('#logMovieButton').click(function(){
-	    rating = $('#rating input').val();
-	    $.ajax({
-	    	type: "POST",
-			url: "getTime.php",
-			success: function(data) {
-				endTime = data; 
-				console.log(endTime);
-				$.ajax({
-					type: "POST",
-					url: "logMovie.php",
-					data: { userNameVal: username, movieVal: movie, ratingVal: rating, startTimeVal: startTime, endTimeVal: endTime },
-					success: function(data){
-						console.log(data);
-					}
-				});
-				$.ajax({
-					type: "POST",
-					url: "makeGraph.php",
-/* 					data: { userNameVal: username, startTimeVal: startTime, endTimeVal: endTime }, */
-					data: { userName: "Mauricio", startTime: 1367524387, endTime: 1367525959},
-					success: function(data) {
-						document.getElementById("graphHolder").innerHTML = data;
-					}
-				});
-				$.ajax({
-					type: "POST",
-					url: "logSuggestion.php",
-					data: { userNameVal: username, movieVal: movie, ratingVal: rating, startTimeVal: startTime, endTimeVal: endTime },
-					success: function(data){
-						document.getElementById("resultsSugHolder").innerHTML = data;
-					}
-				});
-
-			}
-		});
-		document.getElementById("movieName").innerHTML = movie;
-    });
-    
-    $('.returnHome').click(function(){    	
-	    movie = "";
-	    $('#q').val("");
-	    $('#rating input').val("");
-	    $('#rating img').attr("src", "images/stars/star-off.png");
-		startTime = "";
-    	endTime = "";
-		rating = "";
-    	timer = 61;
-    	counting = false;
-    	countDownDone = false;
-    	recievedData = false;
-    	$('#pageCover').hide();
-	    $('#mistakeBox').hide();
-		//set all "val" to "" too	   	
-	   	$.ajax({
-	   		type: "POST",
-			url: "homeData.php",
-			data: { userName: username},
-			success: function(data) {
-				document.getElementById("myDataHolder").innerHTML = data;
-			}
-		});
-		$.ajax({
-			type: "POST",
-			url: "suggestionData.php",
-			data: { userName: username},
-			success: function(data) {
-				document.getElementById("suggestionsHolder").innerHTML = data;
-			}
-		}); 
-    });
-    
-    $(function() {
-    	$.fn.raty.defaults.path = 'images/stars';
-	    $('#rating').raty({
-		  	size     : 24,
-			iconRange: [
-		    	{ range: 1, on: '1.png', off: 'star-off.png' },
-		    	{ range: 2, on: '2.png', off: 'star-off.png' },
-		    	{ range: 3, on: '3.png', off: 'star-off.png' },
-		    	{ range: 4, on: '4.png', off: 'star-off.png' },
-		    	{ range: 5, on: '5.png', off: 'star-off.png' }
-		    ]
-		});
-    }); 
-    
-	$(function(){
-    	$("#q").focus(); //Focus on search field
-    	$("#q").autocomplete({
-        minLength: 0,
-        delay:1,
-        source: "suggest.php",
-        focus: function( event, ui ) {
-            $(this).val( ui.item.label );
-            return false;
-        },
-        select: function( event, ui ) {
-            $(this).val( ui.item.label );
-            return false;
-        }
-    }).data("uiAutocomplete")._renderItem = function( ul, item ) {
-        return $("<li></li>")
-            .data( "item.autocomplete", item )
-            .append( "<a>" + (item.img?"<img class='imdbImage' src='imdbImage.php?url=" + item.img + "' />":"") + "<span class='imdbTitle'>" + item.label + "</span>" + "<div class='clear'></div>" + "</a>")
-            .appendTo( ul );
-            };
-    });	
-    
-    function countdown(){
-		if(counting == true){
-			timer--;
-		}
-		if(timer <= 1){
-			timer = 0;
-			counting = false;
-			countDownDone = true;
-		}
-		document.getElementById("timer").innerHTML = timer;
-	}
-	
-	function fakeClick(event, anchorObj) {
-		if (anchorObj.click) {
-			anchorObj.click()
-		} 
-		else if(document.createEvent) {
-			if(event.target !== anchorObj) {
-				var evt = document.createEvent("MouseEvents"); 
-				evt.initMouseEvent("click", true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null); 
-				var allowDefault = anchorObj.dispatchEvent(evt);
-			      // you can check allowDefault for false to see if
-			      // any handler called evt.preventDefault().
-			      // Firefox will *not* redirect to anchorObj.href
-			      // for you. However every other browser will.
-			}
-		}
+		
+		my_arc.animate({
+		    arc: [circleCenter, circleCenter, amount, circleSize, circleSize]
+		}, 1500, "bounce");
 	}
 
-	function proceed(){
-		if(recievedData){
-			//go to next page
-			console.log(recievedData);
-			fakeClick(event, document.getElementById('rateMovieLink'));
-			recievedData = false;
-			console.log(recievedData);	
-		}
-		else if(countDownDone){
-			$('#pageCover').fadeIn();
-			$('#mistakeBox').slideDown();
-		}
-	}
-	
-	function lookingForData(){
-		if(counting){
-			$.ajax({
-				type: "POST",
-				url: "checkForPulseData.php",
-				data: { userName: username, startTimeVal: startTime},
-				dataType: "json",
-				success: function(data) {
-					if(data.result) {					
-						recievedData = true;
-						counting = false;
-					}
-				}
-			});
-			console.log("trying");	
-		}
-
-/*
-	var bCounting = true;
-	while(bCounting && !countDownDone){
-			$.ajax({
-				type: "POST",
-				url: "checkForPulseData.php",
-				data: { userName: username, startTimeVal: startTime},
-				dataType: "json",
-				success: function(data) {
-					console.log(data);
-					if(data.result) {					
-						bCounting= false;
-					}
-				}
-				
-			});
-		}
-*/
-	}
-
-
-	setInterval(lookingForData, 2000);
-	setInterval(countdown, 1000);
-	setInterval(proceed, 1000);
 });
